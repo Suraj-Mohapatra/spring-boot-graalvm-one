2c6c299ba97070648828aa4ac9c5b3a4c53c18e0
Suraj-Mohapatra <suraj2895@gmail.com>
2025-04-25 02:49:50 +0530

    fixed

--- src/main/java/com/uglyeagle/HomeController.java
+++ src/main/java/com/uglyeagle/HomeController.java
@@ -1,4 +1,4 @@
-package com.ugleagle;
+package com.uglyeagle;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/uglyeagle/SpringBootGraalvmOneApplication.java
+++ src/main/java/com/uglyeagle/SpringBootGraalvmOneApplication.java
@@ -1,4 +1,4 @@
-package com.ugleagle;
+package com.uglyeagle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
